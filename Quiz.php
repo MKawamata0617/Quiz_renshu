6f5a92b4fae9f1b702faba2e8a97820ceb5c9cf0
mamorukawamata <mamoru.kawamata0617@gmail.com>
2020-09-29 18:21:50 +0900

    no message

--- Quiz.php
+++ Quiz.php
@@ -13,6 +13,11 @@ class Quiz {
     }
   }
 
+  public function checkAnswer() {
+    $correctAnswer = $this->_quizSet[$_SESSION['current_num']]['a'][0];
+    return $correctAnswer;
+  }
+
   public function getCurrentQuiz() {
     return $this->_quizSet[$_SESSION['current_num']];
   }
