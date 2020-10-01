99819cdaa9eb39be5aba96388a448fdfc0231a87
mamorukawamata <mamoru.kawamata0617@gmail.com>
2020-10-01 18:17:37 +0900

    10/1学習

--- index.php
+++ index.php
@@ -4,8 +4,11 @@ require_once(__DIR__ . '/config.php');
 require_once(__DIR__ . '/Quiz.php');
 
 $quiz = new MyApp\Quiz();
-$data = $quiz->getCurrentQuiz();
-shuffle($data['a']);
+
+if (!$quiz->isFinished()) {
+  $data = $quiz->getCurrentQuiz();
+  shuffle($data['a']);
+}
 
 ?>
 <!DOCTYPE html>
@@ -16,14 +19,21 @@ shuffle($data['a']);
   <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-  <div id="container">
-    <h1>Q. <?= h($data['q']); ?></h1>
-    <ul>
-      <?php foreach ($data['a'] as $a) : ?>
-        <li class="answer"><?= h($a); ?></li>
-      <?php endforeach; ?>
-    </ul>
-    <div id="btn" class="disabled">Next Question</div>
-  </div>
+  <?php if ($quiz->isFinished()) : ?>
+    <a href="">もう１回</a>
+    <?php $quiz->reset(); ?>
+  <?php else : ?>
+    <div id="container">
+      <h1>Q. <?= h($data['q']); ?></h1>
+      <ul>
+        <?php foreach ($data['a'] as $a) : ?>
+          <li class="answer"><?= h($a); ?></li>
+        <?php endforeach; ?>
+      </ul>
+      <div id="btn" class="disabled">次の問題</div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="quiz.js"></script>
+  <?php endif; ?>
 </body>
 </html>
